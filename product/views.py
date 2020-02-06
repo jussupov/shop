9c4ad642f843dcc4cd2ab82817a7585d086ff81f
jussupov <jus.kz09@gmail.com>
2020-02-06 16:29:19 +0600

    Create product by category endpoint

--- product/views.py
+++ product/views.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import ListProductSerializer, DetailProductSerializer
 from drf_yasg.utils import swagger_auto_schema
 from .models import Product
+from category.models import Category
 from rest_framework.response import Response
 from utilities.pagination import DefaultPagination
 
@@ -14,6 +15,26 @@ class ProductView(ModelViewSet):
     serializer_class = ListProductSerializer
     lookup_field = "slug"
     pagination_class = DefaultPagination
+
+    def list(self, request):
+        if "category" in request.query_params:
+            cat_name = request.query_params.get("category")
+            try:
+                cat = (
+                    Category.objects.get(slug=cat_name)
+                    .child.all()
+                    .values_list("id", flat=True)
+                )
+                products = self.paginate_queryset(
+                    Product.objects.filter(category__id__in=cat)
+                )
+            except Exception:
+                return Response({"detail": "Not found"})
+
+        else:
+            products = self.paginate_queryset(Product.objects.all())
+        serializer_class = ListProductSerializer(products, many=True)
+        return self.get_paginated_response(serializer_class.data)
 
     def retrieve(self, request, slug=None):
         product = get_object_or_404(Product, slug=slug)

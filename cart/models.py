b5958bed78aa9cae017de8e3c587b77169d119e9
jussupov <jus.kz09@gmail.com>
2020-02-16 18:51:54 +0600

    Models for Cart

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from account.models import User
+from product.models import Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
